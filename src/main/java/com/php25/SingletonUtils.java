e45c2c01eb6246abc44c2c0ad6c0b40ae6367c85
penghuiping <575813261@qq.com>
2014-08-10 23:39:36 +0800

    Initial commit

---
+++ src/main/java/com/php25/SingletonUtils.java
@@ -0,0 +1,19 @@
+package com.php25;
+
+
+public abstract class SingletonUtils<T> {
+    private T instance;
+
+    protected abstract T newInstance();
+
+    public final T getInstance() {
+        if (instance == null) {
+            synchronized (SingletonUtils.class) {
+                if (instance == null) {
+                    instance = newInstance();
+                }
+            }
+        }
+        return instance;
+    }
+}
