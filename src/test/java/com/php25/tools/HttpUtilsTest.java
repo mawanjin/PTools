caec8920c10f7a09d91402305068329b42004d10
penghuiping <575813261@qq.com>
2014-09-01 12:54:43 +0800

    .

--- src/test/java/com/php25/tools/HttpUtilsTest.java
+++ src/test/java/com/php25/tools/HttpUtilsTest.java
@@ -1,10 +1,7 @@
 package com.php25.tools;
 
-import com.php25.caches.FileCache;
-import com.sun.deploy.net.HttpResponse;
 import org.junit.Test;
 
-import java.net.ResponseCache;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,29 +11,28 @@ import java.util.Map;
 public class HttpUtilsTest {
 
     @Test
-    public void get() throws Exception{
-        ResponseCache.setDefault(new FileCache(10 * 1024 * 1024L, "/Users/jack/Desktop/cache"));
+    public void get() throws Exception {
+//        ResponseCache.setDefault(new FileCache(10 * 1024 * 1024L, "/Users/jack/Desktop/cache"));
 
-//        HttpUtils.get("http://7.su.bdimg.com/icon/10062.png",null);
-//        HttpUtils.get("http://www.sina.com.cn",null);
-//        HttpUtils.get("http://www.baidu.com",null);
-        System.out.println(HttpUtils.get("http://programmer.csdn.net/",null));
+//         HttpUtils.get("http://7.su.bdimg.com/icon/10062.png",null);
+//         HttpUtils.get("http://www.sina.com.cn",null);
+//         HttpUtils.get("http://www.baidu.com",null);
+//         HttpUtils.get("http://programmer.csdn.net/",null);
 
     }
 
     @Test
     public void post() throws Exception {
-        ResponseCache.setDefault(new FileCache(10 * 1024 * 1024L, "/Users/jack/Desktop/cache"));
-        HttpUtils.post("http://7.su.bdimg.com/icon/10062.png",null);
-       //HttpUtils.post("http://www.sina.com.cn",null);
-       // HttpUtils.post("http://www.baidu.com",null);
+        //ResponseCache.setDefault(new FileCache(10 * 1024 * 1024L, "/Users/jack/Desktop/cache"));
+        //HttpUtils.post("http://7.su.bdimg.com/icon/10062.png",null);
+        // HttpUtils.post("http://www.baidu.com",null);
     }
 
     @Test
     public void getStringParams() {
-        Map<String,String> params = new HashMap<>();
-        params.put("a","123");
-        params.put("b","321");
+        Map<String, String> params = new HashMap<>();
+        params.put("a", "123");
+        params.put("b", "321");
         System.out.println(HttpUtils.getStringParams(params));
     }
 }
